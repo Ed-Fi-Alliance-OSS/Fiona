5666ebe823618f91912d353bd9653d93ada04ef9
Stephen A. Fuqua <stephen@safnet.com>
2026-03-16 17:03:48 -0500

    A dummy test for pipecleaning the test workflow in GitHub

---
+++ apps/fiona-slack/tests/app.test.js
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+describe('app.js', () => {
+	let originalLog;
+
+	beforeEach(() => {
+		originalLog = console.log;
+		console.log = jest.fn();
+		jest.resetModules();
+	});
+
+	afterEach(() => {
+		console.log = originalLog;
+	});
+
+	test('logs hello world on load', () => {
+		require('../src/app');
+		expect(console.log).toHaveBeenCalledWith('hello world');
+	});
+});
+
