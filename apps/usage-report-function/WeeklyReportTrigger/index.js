a5ed4aea218d7d679f640166515ff784fcb1e559
Stephen A. Fuqua <stephen@safnet.com>
2026-04-02 16:41:17 -0500

    Dotenv for the usage report

--- apps/usage-report-function/WeeklyReportTrigger/index.js
+++ apps/usage-report-function/WeeklyReportTrigger/index.js
@@ -3,6 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+import 'dotenv/config';
 import { CosmosClient } from '@azure/cosmos';
 import { app } from '@azure/functions';
 import { DefaultAzureCredential } from '@azure/identity';
